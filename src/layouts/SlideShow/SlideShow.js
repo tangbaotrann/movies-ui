21f1be640d26df9098dcadba6f7599c5442e8d91
tangbaotrann <tangbaotrann@gmail.com>
2022-12-22 04:11:03 +0700

    updated: isLoading

--- src/layouts/SlideShow/SlideShow.js
+++ src/layouts/SlideShow/SlideShow.js
@@ -5,28 +5,34 @@ import { useSelector } from "react-redux";
 
 import styles from "./SlideShow.module.scss";
 import Image from "../../components/Image";
-import { moviesSelector } from "../../redux/selector";
+import { isLoadingAllMovie, moviesSelector } from "../../redux/selector";
+import { CircularProgress } from "@material-ui/core";
 
 const cx = classNames.bind(styles);
 
 function SlideShow() {
   const movies = useSelector(moviesSelector);
+  const isLoadingMovies = useSelector(isLoadingAllMovie);
 
   return (
     <div className="grid wide">
-      <Slide>
-        {movies.map((movie, index) => {
-          return (
-            <div key={index}>
-              <Image
-                className={cx("image-slides")}
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                alt="imageSlides"
-              />
-            </div>
-          );
-        })}
-      </Slide>
+      {isLoadingMovies ? (
+        <CircularProgress />
+      ) : (
+        <Slide>
+          {movies.map((movie, index) => {
+            return (
+              <div key={index}>
+                <Image
+                  className={cx("image-slides")}
+                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                  alt="imageSlides"
+                />
+              </div>
+            );
+          })}
+        </Slide>
+      )}
     </div>
   );
 }
